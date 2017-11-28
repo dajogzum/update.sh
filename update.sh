d56d6dcc4a44bdd23c01f93a44860bfac5efd312
dajogzum <33661462+dajogzum@users.noreply.github.com>
2017-11-28 14:59:31 +0100

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,12 @@
+cd /home/pi/MagicMirror/modules/Traveltime
+echo "Traveltime status:"
+git pull
+cd /home/pi/MagicMirror/modules/NowPlay
+echo "\nNowPlay status:"
+git pull
+cd /home/pi/MagicMirror/modules/AlertsNstats
+echo "\nAlertsNstats status:"
+git pull
+cd /home/pi/MagicMirror/modules/Klimatampio
+echo "\nKlimatampio status:"
+git pull
